9eff5066cd6ef279a89a0a0775deb1780ff71d6b
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-26 15:53:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let chars = ["A", "B", "A", "C", "B"];
 
 function uniq(chars) {
-		let uniqueChars = [...new Set(chars)];
+    let uniqueChars = [...new Set(chars)];
     let sortedUniqueChars = uniqueChars.sort();
     console.log(sortedUniqueChars)
 }
