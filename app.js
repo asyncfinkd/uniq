c844ca78e25854beb48c599df3571fede0d45698
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-26 15:53:37 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let chars = ["A", "B", "A", "C", "B"];
+
+function uniq(chars) {
+		let uniqueChars = [...new Set(chars)];
+    let sortedUniqueChars = uniqueChars.sort();
+    console.log(sortedUniqueChars)
+}
+
+uniq(chars);
